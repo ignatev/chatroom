55d2b27357c6056e14997129834f0b68277a316a
Iskander Ignatev <iskander.ignatev@gmail.com>
2018-06-26 16:59:24 +0300

    add login handler

--- chat/auth.go
+++ chat/auth.go
@@ -1,6 +1,11 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+	"log"
+	"fmt"
+)
 
 type authHandler struct {
 	next http.Handler
@@ -19,6 +24,20 @@ func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 	h.next.ServeHTTP(w, r)
 }
+
 func MustAuth(handler http.Handler) http.Handler {
 	return &authHandler{next: handler}
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	segs := strings.Split(r.URL.Path, "/")
+	action := segs[2]
+	provider := segs[3]
+	switch action {
+	case "login":
+		log.Println("TODO handle login for", provider)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "Auth action %s not supported", action)
+	}
 }
